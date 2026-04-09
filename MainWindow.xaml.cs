c01335ed987a30ac853c4c60eb3707dece9faf95
vitaliikhuda-crypto <vitalii.khuda@student.uzhnu.edu.ua>
2026-04-09 20:33:06 +0300

    Add MainWindow class with initialization message

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace MedSearchWPF.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Тест )
+            MessageBox.Show("Програма запущена!");
+        }
+    }
+}
