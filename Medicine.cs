56e97200dda22545209eac451b91fd7d5646d3f5
vitaliikhuda-crypto <vitalii.khuda@student.uzhnu.edu.ua>
2026-04-16 20:32:14 +0300

    Implement INotifyPropertyChanged in Medicine class
    Refactored Medicine class to implement INotifyPropertyChanged for property change notifications.

--- Medicine.cs
+++ Medicine.cs
@@ -1,16 +1,44 @@
-namespace MedSearchWPF.Models
-{
-    public class Medicine
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Dose { get; set; }
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-        public Medicine(string name, string description, string dose)
-        {
-            Name = name;
-            Description = description;
-            Dose = dose;
-        }
+namespace MedSearchWPF.Models;
+
+/// <summary>
+/// Модель лікарського засобу з INotifyPropertyChanged
+/// </summary>
+public class Medicine : INotifyPropertyChanged
+{
+    private string _name        = string.Empty;
+    private string _description = string.Empty;
+    private string _dose        = string.Empty;
+    private string _icon        = "💊";
+
+    public string Name
+    {
+        get => _name;
+        set { if (_name != value) { _name = value; OnPropertyChanged(); } }
     }
+
+    public string Description
+    {
+        get => _description;
+        set { if (_description != value) { _description = value; OnPropertyChanged(); } }
+    }
+
+    public string Dose
+    {
+        get => _dose;
+        set { if (_dose != value) { _dose = value; OnPropertyChanged(); } }
+    }
+
+    public string Icon
+    {
+        get => _icon;
+        set { if (_icon != value) { _icon = value; OnPropertyChanged(); } }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
